f65a417e76d8590eb7241c4325176d1185f38e42
dahuacai <891676016@qq.com>
2018-10-19 11:02:51 +0800

    简化了marker显示，全部归一到一个markerarray中去.pr

--- navigation-indigo-devel/map_server/src/main.cpp
+++ navigation-indigo-devel/map_server/src/main.cpp
@@ -85,237 +85,185 @@ class MapServer
       return 0;
     }
   void showMapInfo(SimpleGridMap &map){
-
-    goal_marker_pub =  n.advertise<visualization_msgs::Marker>("visualization_goal_marker",1,true);
-    dock_marker_pub = n.advertise<visualization_msgs::Marker>("visualization_dock_marker",1,true);
-    robothome_marker_pub = n.advertise<visualization_msgs::Marker>("visualization_robothome_marker",1,true);
-    //forbiddenarea_marker_pub = n.advertise<visualization_msgs::Marker>("visualization_forbiddenarea_marker",1,true);
-    forbiddenarea_array_marker_pub = n.advertise<visualization_msgs::MarkerArray>("forbiddenarea_MarkerArray", 1,true);
-    text_array_marker_pub = n.advertise<visualization_msgs::MarkerArray>("text_MarkerArray", 1,true);
-    forbiddenline_marker_pub = n.advertise<visualization_msgs::Marker>("visualization_forbiddenline_marker",1,true);
+    int sum = 0;
     uint32_t shape = visualization_msgs::Marker::POINTS;
     uint32_t text_shape = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    map_items_array_pub = n.advertise<visualization_msgs::MarkerArray>("visualization_items_array_marker",1,true);
+    visualization_msgs::Marker items_maker;
     visualization_msgs::Marker text_marker;
-    visualization_msgs::Marker goal_marker;
-    visualization_msgs::Marker dock_marker;
-    visualization_msgs::Marker robothome_marker;
-    visualization_msgs::Marker forbiddenarea_marker;
-    visualization_msgs::MarkerArray forbiddenarea_markerArray;
-    visualization_msgs::MarkerArray text_MarkerArray;
-    visualization_msgs::Marker forbiddenline_marker;
-    int sum = 0;
-
-    
+    visualization_msgs::MarkerArray items_MarkerArray;
     ROS_INFO("map_server.pub dock_points_ size:%d",(map.dock_points_).size() );
+    ///common info for item 
+    items_maker.header.frame_id="/map";
+    items_maker.type = shape;
+    items_maker.ns = "map_dock_space";
+    items_maker.action = visualization_msgs::Marker::ADD;
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    items_maker.scale.x = 0.5;
+    items_maker.scale.y = 0.5;
+    items_maker.scale.z = 0.5;
+    // Set the color -- be sure to set alpha to something non-zero!
+    //DarkOrchid	153 50 204
+    items_maker.color.r = 153;
+    items_maker.color.g = 50;
+    items_maker.color.b = 204;
+    items_maker.color.a = 1.0;
+    items_maker.lifetime = ros::Duration(1000000);
+     ///common info for text 
+
+
     if( map.dock_points_.size() ){
-      dock_marker.points.clear();
+      items_maker.points.clear();
       for(int i=0;i < (map.dock_points_).size();i++){
-        
         OrientPos pos  = ((map.dock_points_)[i]).pos();
         string name =  ((map.dock_points_)[i]).name();
-         //name =  "show time";
         sum+=1;
-        dock_marker.id = sum;
-        dock_marker.header.frame_id="/map";
-        dock_marker.header.stamp = ros::Time::now();
-        dock_marker.ns = "map_dock_space";
-        // Set the marker type
-        dock_marker.type = shape;
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        dock_marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        dock_marker.scale.x = 0.5;
-        dock_marker.scale.y = 0.5;
-        dock_marker.scale.z = 0.5;
-        // Set the color -- be sure to set alpha to something non-zero!
-        //DarkOrchid	153 50 204
-        dock_marker.color.r = 153;
-        dock_marker.color.g = 50;
-        dock_marker.color.b = 204;
-        dock_marker.color.a = 1.0;
-
+        items_maker.id = sum;       
+        items_maker.header.stamp = ros::Time::now();
         geometry_msgs::Point p;
         p.x = pos.x();
         p.y = pos.y();
         p.z = 0;
-
+        items_maker.points.push_back(p);
+       
+        sum+=1;
         geometry_msgs::Pose pose;
         pose.position.x = p.x;
         pose.position.y = p.y;
         pose.position.z = 0;
-
-        dock_marker.points.push_back(p);
-        sum+=1;
-        text_marker = dock_marker;
+        text_marker = items_maker;
         text_marker.ns = "text_space";
+        text_marker.type = text_shape;
         text_marker.color.r = 255;
         text_marker.color.g = 0;
         text_marker.color.b = 0;
         text_marker.color.a = 1;
         text_marker.id = sum;
-        text_marker.type = text_shape;
         text_marker.text = name;
         text_marker.pose = pose;
-        text_MarkerArray.markers.push_back(text_marker);
+        items_MarkerArray.markers.push_back(text_marker);
         ROS_INFO("map_server.pub_dock pos:(%f,%f)",pos.x(),pos.y() );
-        dock_marker.lifetime = ros::Duration(1000000);
+       
       }
-        dock_marker_pub.publish(dock_marker);
+        items_MarkerArray.markers.push_back(items_maker);
     }
     
     ROS_INFO("map_server.pub goals size:%d",(map.goals_).size() );
+    items_maker.ns = "map_goal1_space";
+    //PaleGreen1 154 255 154 for robothome
+    items_maker.color.r = 154;
+    items_maker.color.g = 255;
+    items_maker.color.b = 154;
+    items_maker.color.a = 1.0;
     if((map.goals_).size() ){
-      goal_marker.points.clear();
+      items_maker.points.clear();
       for(int i=0;i < (map.goals_).size();i++){
         
         OrientPos pos  = ((map.goals_)[i]).pos();
         string name =  ((map.goals_)[i]).name();
         sum+=1;
-        goal_marker.id = sum;
-        goal_marker.header.frame_id="/map";
-        goal_marker.header.stamp = ros::Time::now();
-        goal_marker.ns = "map_goal1_space";
-        // Set the marker type
-        goal_marker.type = shape;
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        goal_marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        goal_marker.scale.x = 0.5;
-        goal_marker.scale.y = 0.5;
-        goal_marker.scale.z = 0.5;
-        // Set the color -- be sure to set alpha to something non-zero!
-        //SeaGreen1	84 255 159
-        //AntiqueWhite3	205 192 176
-        //Orange	255 165 0
-        //        //PaleGreen1 154 255 154 for robothome
-        goal_marker.color.r = 154;
-        goal_marker.color.g = 255;
-        goal_marker.color.b = 154;
-        goal_marker.color.a = 1.0;
-
+        items_maker.id = sum;
+        items_maker.header.stamp = ros::Time::now();
+      
         geometry_msgs::Point p;
         p.x = pos.x();
         p.y = pos.y();
         p.z = 0;
+        items_maker.points.push_back(p);
+        sum+=1;
         geometry_msgs::Pose pose;
         pose.position.x = p.x;
         pose.position.y = p.y;
         pose.position.z = 0;
-        goal_marker.points.push_back(p);
-        sum+=1;
-        text_marker = goal_marker;
+        text_marker = items_maker;
         text_marker.ns = "text_space";
+        text_marker.type = text_shape;
         text_marker.color.r = 255;
         text_marker.color.g = 0;
         text_marker.color.b = 0;
         text_marker.color.a = 1;
         text_marker.id = sum;
-        text_marker.type = text_shape;
         text_marker.text = name;
         text_marker.pose = pose;
-        text_MarkerArray.markers.push_back(text_marker);
+        items_MarkerArray.markers.push_back(text_marker);
         ROS_INFO("map_server.pub_dock pos:(%f,%f)",pos.x(),pos.y() );
-        goal_marker.lifetime = ros::Duration(1000000);
+       
       }
-      goal_marker_pub.publish(goal_marker);
+      items_MarkerArray.markers.push_back(items_maker);
     }
     
-
+    items_maker.ns = "map_robothome_space";
+      //Orange	255 165 0 for robothome
+    items_maker.color.r = 255;
+    items_maker.color.g = 165;
+    items_maker.color.b = 0;
+    items_maker.color.a = 1.0;
     ROS_INFO("map_server.pub robot_homes_ size:%d",(map.robot_homes_).size() );
     if((map.robot_homes_).size()){
-      robothome_marker.points.clear();
+      items_maker.points.clear();
       for(int i=0;i < (map.robot_homes_).size();i++){
         OrientPos pos  = ((map.robot_homes_)[i]).pos();
         string name =  ((map.robot_homes_)[i]).name();
         sum+=1;
-        robothome_marker.id = sum;
-        robothome_marker.header.frame_id="/map";
-        robothome_marker.header.stamp = ros::Time::now();
-        robothome_marker.ns = "map_robothome_space";
-        // Set the marker type
-        robothome_marker.type = shape;
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        robothome_marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        robothome_marker.scale.x = 0.5;
-        robothome_marker.scale.y = 0.5;
-        robothome_marker.scale.z = 0.5;
-        // Set the color -- be sure to set alpha to something non-zero!
-
-         //Orange	255 165 0 for robothome
-        robothome_marker.color.r = 255;
-        robothome_marker.color.g = 165;
-        robothome_marker.color.b = 0;
-        robothome_marker.color.a = 1.0;
-
+        items_maker.id = sum;
+        items_maker.header.stamp = ros::Time::now();
         geometry_msgs::Point p;
         p.x = pos.x();
         p.y = pos.y();
         p.z = 0;
+        items_maker.points.push_back(p);
+
+        sum+=1;
         geometry_msgs::Pose pose;
         pose.position.x = p.x;
         pose.position.y = p.y;
         pose.position.z = 0;
-        robothome_marker.points.push_back(p);
-        sum+=1;
-        text_marker = robothome_marker;
+        text_marker = items_maker;
         text_marker.ns = "text_space";
+        text_marker.type = text_shape;
         text_marker.color.r = 255;
         text_marker.color.g = 0;
         text_marker.color.b = 0;
         text_marker.color.a = 1;
         text_marker.id = sum;
-        text_marker.type = text_shape;
         text_marker.text = name;
         text_marker.pose = pose;
-        text_MarkerArray.markers.push_back(text_marker);
+        items_MarkerArray.markers.push_back(text_marker);
         ROS_INFO("map_server.pub goal_ pos:(%f,%f)",pos.x(),pos.y() );
-        robothome_marker.lifetime = ros::Duration(1000000);
       }
-      robothome_marker_pub.publish(robothome_marker);
+      items_MarkerArray.markers.push_back(items_maker);
     }
     
 
     shape = visualization_msgs::Marker::LINE_STRIP;
-    forbiddenarea_marker.type = shape;
+    items_maker.type = shape;//shape chageed
+    items_maker.ns = "map_forbiddenarea_space";
+    // Set the marker type
+    
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    //Line strips also have some special handling for scale:
+    //only scale.x is used and it controls the width of the line segments.
+    items_maker.scale.x =0.1;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    //VioletRed4	139 34 82
+    items_maker.color.r = 139;
+    items_maker.color.g = 34;
+    items_maker.color.b = 82;
+    items_maker.color.a = 1.0;
     ROS_INFO("map_server.pub forbidden_area_ size:%d",(map.forbidden_area_).size() );
-    if( (map.forbidden_area_).size() ){
-      forbiddenarea_markerArray.markers.clear();    
+    if( (map.forbidden_area_).size() ){ 
       for(int i=0;i < (map.forbidden_area_).size();i++){
-        forbiddenarea_marker.points.clear();
+        items_maker.points.clear();
         FreeRegion1 reg  = ((map.forbidden_area_)[i]).regin();
         string icon = ((map.forbidden_area_)[i]).name();
         OrientPos cen_pos = ((map.forbidden_area_)[i]).pos();
         std::vector<VecPosition> list = reg.list();
         sum += 1;
-        forbiddenarea_marker.header.stamp = ros::Time::now();
-        forbiddenarea_marker.id = sum;
-        forbiddenarea_marker.header.frame_id="/map";
-        forbiddenarea_marker.header.stamp = ros::Time::now();
-        forbiddenarea_marker.ns = "map_forbiddenarea_space";
-        // Set the marker type
-        forbiddenarea_marker.type = shape;
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        forbiddenarea_marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        //Line strips also have some special handling for scale:
-        //only scale.x is used and it controls the width of the line segments.
-        forbiddenarea_marker.scale.x =0.1;
-
-        // Set the color -- be sure to set alpha to something non-zero!
-        //VioletRed4	139 34 82
-        forbiddenarea_marker.color.r = 139;
-        forbiddenarea_marker.color.g = 34;
-        forbiddenarea_marker.color.b = 82;
-        forbiddenarea_marker.color.a = 1.0;
-
+        items_maker.header.stamp = ros::Time::now();
+        items_maker.id = sum;
         geometry_msgs::Point p;
         p.z = 0;
-        
         geometry_msgs::Pose pose;
         pose.position.x = 0.5*(list[0].getX()+list[2].getX());
         pose.position.y = 0.5*(list[0].getY()+list[2].getY());
@@ -323,66 +271,63 @@ class MapServer
         for(int j =0; j < list.size();j++){
           p.x = list[j].getX();
           p.y = list[j].getY();
-          forbiddenarea_marker.points.push_back(p);
+          items_maker.points.push_back(p);
         }
         //add first to end line
         p.x = list[0].getX();
         p.y = list[0].getY();
-        forbiddenarea_marker.points.push_back(p);
+        items_maker.points.push_back(p);
         sum+=1;
-        text_marker = forbiddenarea_marker;
+        text_marker = items_maker;
         text_marker.ns = "text_space";
-        text_marker.scale.x = 0.5;
-        text_marker.scale.y = 0.5;
-        text_marker.scale.z = 0.5;
+        text_marker.type = text_shape;
+        text_marker.scale.x =0.5;
+        text_marker.scale.x =0.5;
+        text_marker.scale.x =0.5;
         text_marker.color.r = 255;
         text_marker.color.g = 0;
         text_marker.color.b = 0;
         text_marker.color.a = 1;
         text_marker.id = sum;
-        text_marker.type = text_shape;
         text_marker.text = icon;
         text_marker.pose = pose;
-        text_MarkerArray.markers.push_back(text_marker);
-        forbiddenarea_marker.lifetime =  ros::Duration(1000000);
-        forbiddenarea_markerArray.markers.push_back(forbiddenarea_marker);
+        items_MarkerArray.markers.push_back(text_marker);
+        items_MarkerArray.markers.push_back(items_maker);
       }
-      forbiddenarea_array_marker_pub.publish(forbiddenarea_markerArray);
+      
     }
     
     //Line lists use the points member of the visualization_msgs/Marker message.
     //It will draw a line between each pair of points, so 0-1, 2-3, 4-5, ...
     shape = visualization_msgs::Marker::LINE_LIST;
+    items_maker.type = shape;
+    items_maker.ns = "map_forbidden_line_space";
+    // Set the marker type
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    //Line strips also have some special handling for scale:
+    //only scale.x is used and it controls the width of the line segments.
+    items_maker.scale.x = 0.1;
+    // DarkGoldenrod1	255 185 15
+    items_maker.color.r = 255;
+    items_maker.color.g = 185;
+    items_maker.color.b = 15;
+    items_maker.color.a = 1;
     ROS_INFO("map_server.pub forbidden_line_ size:%d",(map.forbidden_line_).size() );
     if((map.forbidden_line_).size()){
-      forbiddenline_marker.points.clear();
+      items_maker.points.clear();
       for(int i=0;i < (map.forbidden_line_).size();i++){
         OrientPos s_pos,e_pos;
         ((map.forbidden_line_)[i]).getPairPos(s_pos,e_pos);
         string icon = ((map.forbidden_line_)[i]).name();
         sum += 1;
-        forbiddenline_marker.header.stamp = ros::Time::now();
-        forbiddenline_marker.id = sum;
-        forbiddenline_marker.header.frame_id="/map";
-        forbiddenline_marker.ns = "map_forbidden_line_space";
-        // Set the marker type
-        forbiddenline_marker.type = shape;
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        forbiddenline_marker.action = visualization_msgs::Marker::ADD;
-        //Line strips also have some special handling for scale:
-        //only scale.x is used and it controls the width of the line segments.
-        forbiddenline_marker.scale.x = 0.1;
-        // DarkGoldenrod1	255 185 15
-        forbiddenline_marker.color.r = 255;
-        forbiddenline_marker.color.g = 185;
-        forbiddenline_marker.color.b = 15;
-        forbiddenline_marker.color.a = 1;
+        items_maker.header.stamp = ros::Time::now();
+        items_maker.id = sum;
         geometry_msgs::Point p;
         p.z = 0;
         //add first to end line
         p.x = s_pos.x();
         p.y = s_pos.y();
-        forbiddenline_marker.points.push_back(p);
+        items_maker.points.push_back(p);
         p.x = e_pos.x();
         p.y = e_pos.y();
         
@@ -391,27 +336,30 @@ class MapServer
         pose.position.y = 0.5*(s_pos.y()+e_pos.y());
         pose.position.z = 0;
 
-        forbiddenline_marker.points.push_back(p);
+        items_maker.points.push_back(p);
         sum+=1;
-        text_marker = forbiddenline_marker;
+        text_marker = items_maker;
         text_marker.ns = "text_space";
-        text_marker.scale.x = 0.5;
-        text_marker.scale.y = 0.5;
-        text_marker.scale.z = 0.5;
+        text_marker.type = text_shape;
+        text_marker.scale.x =0.5;
+        text_marker.scale.x =0.5;
+        text_marker.scale.x =0.5;
         text_marker.color.r = 255;
         text_marker.color.g = 0;
         text_marker.color.b = 0;
         text_marker.color.a = 1;
         text_marker.id = sum;
-        text_marker.type = text_shape;
         text_marker.text = icon;
         text_marker.pose = pose;
-        text_MarkerArray.markers.push_back(text_marker);
-        forbiddenline_marker.lifetime = ros::Duration(1000000);
+        items_MarkerArray.markers.push_back(text_marker); 
       }
-      forbiddenline_marker_pub.publish(forbiddenline_marker); 
+      items_MarkerArray.markers.push_back(items_maker);
     }
-     text_array_marker_pub.publish(text_MarkerArray); 
+    if(items_MarkerArray.markers.size()){
+      std::cout << "map_server.show map item info.marker size: " << items_MarkerArray.markers.size() << std::endl;
+      map_items_array_pub.publish(items_MarkerArray); 
+    }
+    
   }
 
     /** Trivial constructor */
@@ -586,14 +534,8 @@ class MapServer
     SimpleGridMap map;
     ros::Publisher map_pub;
     ros::Publisher metadata_pub;
-    ros::Publisher goal_marker_pub;
-    ros::Publisher text_array_marker_pub;
-    ros::Publisher dock_marker_pub;
-    ros::Publisher robothome_marker_pub;
-    ros::Publisher forbiddenarea_marker_pub;
-    ros::Publisher forbiddenarea_array_marker_pub;
-    ros::Publisher forbiddenline_marker_pub;
     ros::ServiceServer service;
+    ros::Publisher map_items_array_pub;
     bool deprecated;
 
     /** Callback invoked when someone requests our service */
