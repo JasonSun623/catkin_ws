da74fcb14841596ac0bf165427021bf0fe977c17
dahuacai <891676016@qq.com>
2019-01-29 10:34:04 +0800

    rm file -r buiud

--- navigation-indigo-devel/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/hdros/catkin_ws/src/navigation-indigo-devel/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/hdros/catkin_ws/src/navigation-indigo-devel/devel;/opt/ros/indigo".split(';') if "/home/hdros/catkin_ws/src/navigation-indigo-devel/devel;/opt/ros/indigo" != "" else []
